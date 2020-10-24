84d714364b5c37c6bd3622b4f0d0867c8e2b7cb1
Robert Schmahl <rschm007@gmail.com>
2020-10-24 15:40:02 -0700

    icons now work

--- Assets/script.js
+++ Assets/script.js
@@ -17,8 +17,12 @@ $(document).ready(function () {
     method: "GET",
   }).then(function (response) {
     console.log(response);
+
+    // gather the current weather to make into an icon
+    let weatherIcon = response.weather[0].icon;
+
     // insert the LocationDate DOM object
-    $(".LocationDate").text(response.name + " " + now);
+    $(".LocationDate").text(response.name + " (" + now + ")");
 
     // insert the Temperature DOM object
     $(".temperature").text(
@@ -33,6 +37,66 @@ $(document).ready(function () {
 
     // insert the wind speed DOM object
     $(".wind").text("Wind Speed: " + response.wind.speed + " MPH");
+
+    // define a function that changes the weatherIcon in the LocationDate icon
+    function weatherIconDOM(weatherIcon) {
+      // clear sky group
+      if (weatherIcon === "01d") {
+        $(".weatherIcon").attr(
+          "src",
+          "https://cdn0.iconfinder.com/data/icons/weather-filled-outline-6/64/weather_cloud_sun_moon_rain-49-512.png"
+        );
+        // few clouds group
+      } else if (weatherIcon === "02d") {
+        $(".weatherIcon").attr(
+          "src",
+          "https://cdn0.iconfinder.com/data/icons/weather-filled-outline-6/64/weather_cloud_sun_moon_rain-04-512.png"
+        );
+        // scattered clouds group
+      } else if (weatherIcon === "03d") {
+        $(".weatherIcon").attr(
+          "src",
+          "https://cdn0.iconfinder.com/data/icons/weather-filled-outline-6/64/weather_cloud_sun_moon_rain-41-512.png"
+        );
+        // broken clouds group
+      } else if (weatherIcon === "04d") {
+        $(".weatherIcon").attr(
+          "src",
+          "https://cdn0.iconfinder.com/data/icons/weather-filled-outline-6/64/weather_cloud_sun_moon_rain-05-512.png"
+        );
+        // shower rain group
+      } else if (weatherIcon === "09d") {
+        $(".weatherIcon").attr(
+          "src",
+          "https://cdn0.iconfinder.com/data/icons/weather-filled-outline-6/64/weather_cloud_sun_moon_rain-02-512.png"
+        );
+        // rain group
+      } else if (weatherIcon === "10d") {
+        $(".weatherIcon").attr(
+          "src",
+          "https://cdn0.iconfinder.com/data/icons/weather-filled-outline-6/64/weather_cloud_sun_moon_rain-03-512.png"
+        );
+        // thunderstorm group
+      } else if (weatherIcon === "11d") {
+        $(".weatherIcon").attr(
+          "src",
+          "https://www.iconfinder.com/icons/5896932/moon_cloud_sun_weather_rain_icon"
+        );
+        // snow group
+      } else if (weatherIcon === "13d") {
+        $(".weatherIcon").attr(
+          "src",
+          "https://cdn0.iconfinder.com/data/icons/weather-filled-outline-6/64/weather_cloud_sun_moon_rain-21-512.png"
+        );
+        // mist group
+      } else {
+        $(".weatherIcon").attr(
+          "src",
+          "https://cdn0.iconfinder.com/data/icons/weather-filled-outline-6/64/weather_cloud_sun_moon_rain-18-512.png"
+        );
+      };
+    };
+    weatherIconDOM(weatherIcon);
 
     // gathering vars for the UV Index ajax call
 
@@ -68,10 +132,8 @@ $(document).ready(function () {
         } else {
           $(".uvIndex").addClass("bg-purple-800");
         }
-      };
-
+      }
       uvIndexSeverity(uvIndexValue);
-
     });
   });
 });
